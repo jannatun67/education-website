92ac8172a2e316ee16246167db007c4b79f30c7f
jannatun67 <jannatunfima67@gmail.com>
2025-05-06 22:46:03 +0600

    added banner

--- src/Componente/Banner.jsx
+++ src/Componente/Banner.jsx
@@ -4,22 +4,23 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 import { EffectCards,Pagination } from "swiper/modules";
 import 'swiper/css/pagination';
+import { FaArrowRightLong } from "react-icons/fa6";
 
 import banner1 from "../assets/Banner/Item_06.png"
 import banner2 from "../assets/Banner/Item_04.png"
+import banner3 from "../assets/Banner/Item_01.png"
 
 const Banner = () => {
   return (
-    <div className="bg-purple-100 py-10">
+    <div className="bg-purple-100 py-16 px-3 ">
       <div className="grid md:grid-cols-3 grid-cols-1 gap-3 md:w-[1200px] mx-auto">
-        <div className="md:col-span-2 col-span-1">
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="md:col-span-2 col-span-1  md:w-[599px] md:mt-24 ">
+            <p className="text-[#5F2DED] py-1  bg-[#DED4F7] rounded-[20px]  w-[257px] text-center" >The Leader in Online Learning</p>
+          <h1 className="md:text-6xl text-2xl font-bold">Grow Your Learning Skills With Us</h1>
+          <p className="py-3 ">
+          Unlock your full potential and achieve your personal and professional goals through our diverse range of courses and expert-led programs. 
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">View Course <FaArrowRightLong /></button>
         </div>
 
         <div>
@@ -34,15 +35,10 @@ const Banner = () => {
              
           >
             
-            <SwiperSlide><img src={banner1} alt="" /></SwiperSlide>
-            <SwiperSlide><img src={banner2} alt="" /></SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
+            <SwiperSlide><img className="md:w-[384px] w-[300px] object-cover" src={banner1} alt="" /></SwiperSlide>
+            <SwiperSlide><img className="md:w-[384px] w-[300px]  object-cover"  src={banner2} alt="" /></SwiperSlide>
+            <SwiperSlide><img className="md:w-[384px] w-[300px]  object-cover"  src={banner3} alt="" /></SwiperSlide>
+        
           </Swiper>
         </div>
       </div>
